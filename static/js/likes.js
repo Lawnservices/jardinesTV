bd945402aaacafcad781570548ed84bc58ea5280
Miguel Laguna <80590513+Lawnservices@users.noreply.github.com>
2026-07-30 14:42:21 -0400

    fix

--- static/js/likes.js
+++ static/js/likes.js
@@ -14,30 +14,46 @@
 // }
 
 async function darLike(id) {
-    const res = await fetch(`https://www.creantunegocio.com/api/videos/${id}/like`, { method: "POST" });
+
+    const res = await fetch(
+        `https://www.creantunegocio.com/api/videos/${id}/like`,
+        {
+            method: "POST"
+        }
+    );
+
     const data = await res.json();
+
 
     const btn = document.getElementById("btnLike");
 
-    if (!data.ok) {
-        // Ya dio like → cambiar botón
+
+    if (res.ok) {
+
         btn.textContent = "❤️ Gracias";
         btn.classList.add("disabled");
         btn.disabled = true;
-    } else {
-        // Like nuevo → cambiar botón
-        btn.textContent = "❤️ Gracias";
-        btn.classList.add("disabled");
-        btn.disabled = true;
+
     }
+
 
     cargarLikes(id);
 }
 
+
+
 async function cargarLikes(id) {
-    const res = await fetch(`https://www.creantunegocio.com/api/videos/${id}/like`);
+
+    const res = await fetch(
+        `https://www.creantunegocio.com/api/videos/${id}/likess`
+    );
+
+
     const data = await res.json();
+
+
     document.getElementById("totalLikes").textContent = data.likes;
+
 }
 
 // Cargar likes al entrar
