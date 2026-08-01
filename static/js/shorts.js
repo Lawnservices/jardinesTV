abc1828948562b8371935bba1cbec21d4e1e6a66
Miguel Laguna <80590513+Lawnservices@users.noreply.github.com>
2026-07-31 21:38:22 -0400

    fix

---
+++ static/js/shorts.js
@@ -0,0 +1,28 @@
+const videos = document.querySelectorAll(".video");
+
+const observer = new IntersectionObserver((entries)=>{
+
+entries.forEach(entry=>{
+
+const video = entry.target;
+
+if(entry.isIntersecting){
+
+video.play();
+
+}else{
+
+video.pause();
+
+}
+
+});
+
+},{
+threshold:0.7
+});
+
+
+videos.forEach(video=>{
+observer.observe(video);
+});
